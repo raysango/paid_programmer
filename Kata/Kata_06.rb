eb4b5a11670911820cf8678922b839536186d0e7
Raymond Morcos <remongeorge@live.com>
2015-08-06 23:13:05 -0700

    kata06 update

--- Kata/Kata_06.rb
+++ Kata/Kata_06.rb
@@ -2,7 +2,7 @@ def values_permutation(n, t, f)
 	x, output, y= [], [], []
 	x = [n, t, f].permutation(3).to_a
 	x.each do |e| 
-		output.push(e.join('&&'),e.join('||'),e.zip(['&&','||']).flatten.compact.join, e.zip(['||','&&']).flatten.compact.join)
+		output.push(e.join('&&'),e.join('||'),e.zip(['&&','||']).flatten.join, e.zip(['||','&&']).flatten.join)
 	end
 	output.each_slice(1).each.map {|e| y.push(e)}
 	return y.each_slice(4).each.map {|e| e.flatten}
