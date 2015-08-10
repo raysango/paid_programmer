104c27fb6466c8ab0309787736db34499b79b578
Raymond Morcos <remongeorge@live.com>
2015-08-09 19:36:59 -0700

    update

--- Kata/permutation.rb
+++ Kata/permutation.rb
@@ -19,7 +19,7 @@
 
 def values_permutation(n, t, f)
 	def to_sb
-    return 'nil' if [FalseClass, NilClass].include?(self.class) 
+    return 'nil' if NilClass == self.class
   end  
 	x, output, y= [], [], []
 	x = [n.to_sb, t, f].permutation(3).to_a
@@ -27,7 +27,7 @@ def values_permutation(n, t, f)
 		output.push(e.join('&&'),e.join('||'),e.zip(['&&','||']).flatten.join, e.zip(['||','&&']).flatten.join)
 	end
 	output.each.map {|e| y.push(e) && y.push(instance_eval(e))}
-	print y.each_slice(2).each.map{|e| e}
+	return y.each_slice(2).each.map{|e| e}
 end
 
 values_permutation(nil, true, false)
---
+++ Kata/transpose.rb
@@ -0,0 +1,17 @@
+# write a method called "transpose" that takes this array of arrays:
+
+[
+['first', 'second'],
+['third', 'fourth']
+]
+
+# and transposes it into this array of arrays
+
+[
+['first', 'third'],
+['second', 'fourth']
+]
+
+# you can think of it as rows become columns
+# I know there is an Array method called "transpose" but
+# I am asking you to write your own
